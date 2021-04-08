9d6eaca2d5c46066f8f640fbc45ba908634e432f
Joao Guimaraes <joao.guimaraes@devexo.com.br>
2021-04-07 21:31:00 -0300

    [ADD] tratamento de datas moment

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -1,16 +1,47 @@
 const conexao = require('../infra/conexao')
-
+const moment = require('moment')
 class Atendimento {
-    adiciona(atendimento) {
+    adiciona(atendimento, res) {
+        const dataCriacao = moment().format('YYYY-MM-DD HH:MM:SS')
+        const data = moment(atendimento.data).format('YYYY-MM-DD HH:MM:SS')
+
+        const dataEhValida = moment(data).isSameOrAfter(dataCriacao)
+        const clienteEhValido = atendimento.cliente.length >= 5
+
+        const validacoes = [
+            {
+                nome: 'data',
+                valido: dataEhValida,
+                mensagem: 'Data deve ser maior ou igual a data atual'
+
+            },
+            {
+                nome: 'cliente',
+                valido: clienteEhValido,
+                mensagem: 'Cliente deve ter pelo menos 5 caracteres'
+            }
+        ]
+
+        const erros = validacoes.filter(campo => !campo.valido)
+        const existemErros = erros.length
+
+        if(existemErros) {
+            res.status(400).json(erros)
+        } else {
+            const atendimentoDatado = { ...atendimento, dataCriacao, data }
+
         const sql = 'INSERT INTO Atendimentos SET ?'
 
-        conexao.query(sql, atendimento, (erro, resultados) => {
+        conexao.query(sql, atendimentoDatado, (erro, resultados) => {
             if(erro) {
-                console.log(erro)
+                res.status(400).json(erro)
             } else {
-                console.log(resultados)
+                res.status(201).json(resultados)
             }
         })
+        }
+
+        
     }
 }
 
