ec6adcf0e9aa0a238d110d59f8250933d0d93e7c
Joao Guimaraes <joao.guimaraes@devexo.com.br>
2021-04-13 16:41:25 -0300

    [ADD] manipulacao de arquivo no server

---
+++ models/pets.js
@@ -0,0 +1,32 @@
+const conexao = require('../infra/conexao')
+const uploadDeArquivo = require('../arquivos/uploadDeArquivos')
+
+class Pet {
+    adicionaPet(pet, res) {
+        const query = 'INSERT INTO Pets SET ?'
+
+        uploadDeArquivo(pet.imagem, pet.nome, (erro, novoCaminho) => {
+
+            if(erro) {
+                res.status(400).json({ erro })
+            } else {
+                const novoPet = { nome: pet.nome, imagem: novoCaminho }
+                conexao.query(query, novoPet, erro => {
+                    if(erro) {
+                        console.log(erro)
+                        res.status(400).json(erro)
+                    } else {
+                        res.status(200).json(novoPet)
+                    }
+                })
+            }
+            
+
+
+        })
+
+
+    }
+}
+
+module.exports = new Pet()
